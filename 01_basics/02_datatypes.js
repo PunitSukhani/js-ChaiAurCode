de5adf58307a5d07fe8c15e11f8d49c034ee173a
PunitSukhani <sukhanipunit@gmail.com>
2024-07-17 18:33:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" ; // use all JS code as newer version 
+
+/*
+number => upto 2 ^ 53
+bigint = for large numbers
+string = ""
+boolean = true/false
+null = standalone value (type = object)
+undefined (type = undefined)
+symbol = uniquness
+
+object
+*/
+
+console.log(typeof null) 
+console.log(typeof undefined)
