1802ff7bc69659a14b2489bb7c8676fc760603b2
PunitSukhani <sukhanipunit@gmail.com>
2024-07-17 19:41:51 +0000

    datatypes theory

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -14,3 +14,21 @@ object
 
 console.log(typeof null) 
 console.log(typeof undefined)
+
+/*
+1) Primitive Datatypes (by value)
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes (by refrence)
+       Arrays  =>  object
+       Function  =>  function object
+       Object  =>  object
+*/
+
+// JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime
