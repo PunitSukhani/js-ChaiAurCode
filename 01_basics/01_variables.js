6aafd3d1516319a8bd8f91ea03445aecea72ae83
PunitSukhani <sukhanipunit@gmail.com>
2024-07-17 18:19:03 +0000

    varibales

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const Id = 123 //cant be changed
+let AccName = "Punit" // can be changed 
+
+let number = 100 
+
+/* 
+var city  = "Hisar" 
+console.log(city); 
+
+Var not used because of issue of scope
+*/
+
+number = 99
+console.log(number);
+
+let email 
+
+console.table([Id, AccName, number, email])
