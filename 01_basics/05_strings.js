912935a5f66bca0623b232194e18a6a1a57e3c55
PunitSukhani <sukhanipunit@gmail.com>
2024-07-18 20:35:02 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+
+const age = 19
+
+console.log(`My age is ${age}`) // To print numbers 
+
+let gameName = "Punit Sukhani"
+
+console.log(gameName.length)
+
+console.log(gameName.toUpperCase())
+
+console.log(gameName); // No permanent change 
+
+gameName = gameName.toUpperCase() // to implement permanent change  
+
+console.log(gameName)
+
+console.log(gameName.substring(0,5))
+
+
